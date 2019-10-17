d413d2700a394755804300340566c7b44598c3ab
Divine Mbunga <divinembunga7@gmail.com>
2019-10-17 11:58:33 +0100

    added test for empty and non Dag- tests are all complete and working

--- src/lcaDagTest.java
+++ src/lcaDagTest.java
@@ -25,7 +25,6 @@ public class lcaDagTest {
 	public void testAddEdge(){
 		lcaDag testEdge = new lcaDag(5);
 		testEdge.addEdge(0, 1);
-		//test4.addEdge(-2, -5);
 		
 		assertEquals("Testing edge count is 1", 1, testEdge.E());
 		
@@ -57,6 +56,24 @@ public class lcaDagTest {
 		assertFalse(acyclic.hasCycle());
 	}
 	
+	@Test
+	public void testLCAForNonDAG(){
+		lcaDag nonAcyclic = new lcaDag(11);
+		
+		nonAcyclic.addEdge(0, 1);
+		nonAcyclic.addEdge(1, 2);
+		nonAcyclic.addEdge(2, 0);
+		nonAcyclic.addEdge(1, 3);
+		nonAcyclic.addEdge(3, 2);
+		nonAcyclic.addEdge(2, 4); 
+		nonAcyclic.addEdge(4, 3);
+		
+		//Should return -1 if graph is not a DAG
+		assertEquals("", -1, nonAcyclic.findLCA(2, 3));
+		assertEquals("", -1, nonAcyclic.findLCA(3, 4));
+		assertEquals("", -1, nonAcyclic.findLCA(1, 2));
+		assertEquals("", -1, nonAcyclic.findLCA(0, 3));
+		}
 	
 	@Test
 	public void testLCAForNoCommonAncestors(){
@@ -88,7 +105,13 @@ public class lcaDagTest {
 	public void exceptionTest(){
 		//Can't make a directed graph with less than 0 vertices
 		lcaDag testException = new lcaDag(-4);
-	}			
+	}	
+	
+	@Test
+	public void testLCAforEmpty() {
+		lcaDag lcaEmpty = new lcaDag(9);
+		assertEquals("Testing LCA is -1", -1, lcaEmpty.findLCA(0, 2));
+	}
 	
 	@Test
 	public void testLCA(){
